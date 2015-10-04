137aa67ecd149fbda385bc6423329fd385bbe9a4
Guillaume Carayon <guillaume.carayon@ensc.fr>
2015-10-04 18:23:24 +0200

    butterfly et liaison avec perso

---
+++ Assets/Scripts/ClickButterfly.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickButterfly : MonoBehaviour
+{
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+    GameObject butterfly = GameObject.FindGameObjectWithTag("Butterfly");
+    void Start()
+    {
+        butterfly.AddComponent<FixedJoint>();
+        butterfly.GetComponent<FixedJoint>().connectedBody = player.GetComponent<Rigidbody>();
+    }
+}
